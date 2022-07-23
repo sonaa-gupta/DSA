861e2bebb56344004378f43d57a8b8ece3e672df
sonaa-gupta <55847679+sonaa-gupta@users.noreply.github.com>
2022-07-24 01:01:52 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (63.50%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumMoves(String s) {
+        
+        int ans=0,i=0;
+      while(i<s.length())
+        {
+            if(s.charAt(i)=='X'){
+                ans++;i+=3;}
+            else 
+                i++;
+        }
+        return ans;
+    }
+}
