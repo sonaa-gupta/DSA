cac8fa563c00e290ff8ef35ca1c3296c023bc0ec
sonaa-gupta <55847679+sonaa-gupta@users.noreply.github.com>
2022-08-25 22:33:34 +0530

    Time: 1 ms (78.53%), Space: 51 MB (55.77%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=1;i<=n;i++)
+          sum+=i;
+        int sum2=0;
+        for(int i=0;i<n;i++)
+            sum2+=nums[i];
+        return sum-sum2;
+        }
+}
